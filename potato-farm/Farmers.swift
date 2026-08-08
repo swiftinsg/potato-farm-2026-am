384251a0dc1f569c3b493d0fbf2710018dbfbc79
chengzhangchye <chyechengzhang@gmail.com>
2026-08-08 10:12:53 +0800

    feat: plant Cheng Zhang in plot 26

--- potato-farm/Farmers.swift
+++ potato-farm/Farmers.swift
@@ -43,5 +43,5 @@ let potatoes: [Potato] = [
     Potato(plot: 4, name: "Tristan", color: .indigo),
 
     // ── Students: add your line below this one! ───────────────────
-
+    Potato(plot: 26, name: "Cheng Zhang", color: .green)
 ]
